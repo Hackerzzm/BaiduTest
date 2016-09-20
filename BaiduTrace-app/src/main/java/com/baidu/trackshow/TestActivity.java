383cec0a026ba77f1211843f6186ff2fcc9bb9a4
unknown <zheming.zou@chall.zxtech.com>
2016-09-20 16:21:57 +0800

    Initial commit

---
+++ BaiduTrace-app/src/main/java/com/baidu/trackshow/TestActivity.java
@@ -0,0 +1,13 @@
+package com.baidu.trackshow;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class TestActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
